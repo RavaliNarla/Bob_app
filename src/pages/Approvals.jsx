33f9e627eb51a51ce4bb4789604546377d6bea4b
RavaliNarla <your-email@example.com>
2025-09-09 15:22:19 +0530

    update code

--- src/pages/Approvals.jsx
+++ src/pages/Approvals.jsx
@@ -77,7 +77,8 @@ const Approvals = () => {
     setError(null);
     try {
       // Pass the user's role to the API service
-      const responseData = await apiService.getApprovalstatus(user.role);
+      console.log("User role:", user);
+      const responseData = await apiService.getApprovalstatus(user.userid);
       if (responseData && Array.isArray(responseData.data)) {
         setJobPostings(responseData.data);
       } else {
@@ -140,15 +141,13 @@ setError("No Approvals: Unexpected data format.");
     }
 
     const payload = {
-      role: user.role,
-      requisitionId: selectedJobIds,
+    
+      requisitionIdList: selectedJobIds,
       status: "APPROVED",
       description: rejectDescription,
-      userid: user.userid,
+      userId: user.userid,
     };
-
     
-
     try {
       await apiService.updateApproval(payload);
       toast.success(`Approved ${selectedJobIds.length} requisitions`);
